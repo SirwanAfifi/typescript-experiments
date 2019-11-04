87b795b3f45572ac5bd978f211dec13d7d94f2e2
sirwan <sir1afifi@gmail.com>
2019-11-04 10:04:26 +0330

    public is considered as default

--- todo/src/todoItem.ts
+++ todo/src/todoItem.ts
@@ -5,7 +5,7 @@ export class TodoItem {
     public complete: boolean = false
   ) {}
 
-  public printDetails(): void {
+  printDetails(): void {
     console.log(
       `${this.id}\t${this.task} ${this.complete ? "\t(complete)" : ""}`
     );
