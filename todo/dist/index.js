f0e0eb2d463470646cda27b09c56c4b12c2c477a
sirwan <sir1afifi@gmail.com>
2019-11-04 10:38:06 +0330

    Use Map

--- todo/dist/index.js
+++ todo/dist/index.js
@@ -1,2 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+let todos = [
+    new todoItem_1.TodoItem(1, "Buy Flowers"),
+    new todoItem_1.TodoItem(2, "Get Shoes"),
+    new todoItem_1.TodoItem(3, "Collect Tickets"),
+    new todoItem_1.TodoItem(4, "Call Joe", true)
+];
+let collection = new todoCollection_1.TodoCollection("Sirwan", todos);
 console.clear();
-console.log("Sirwan's Todo List");
+console.log(`${collection.userName}'s Todo List ` +
+    `(${collection.getItemCounts().incomplete} items to do)`);
+// collection.removeComplete();
+collection.getTodoItems(true).forEach(item => item.printDetails());
--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,2 +1,20 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos = [
+  new TodoItem(1, "Buy Flowers"),
+  new TodoItem(2, "Get Shoes"),
+  new TodoItem(3, "Collect Tickets"),
+  new TodoItem(4, "Call Joe", true)
+];
+
+let collection = new TodoCollection("Sirwan", todos);
+
 console.clear();
-console.log("Sirwan's Todo List");
+console.log(
+  `${collection.userName}'s Todo List ` +
+    `(${collection.getItemCounts().incomplete} items to do)`
+);
+
+// collection.removeComplete();
+collection.getTodoItems(true).forEach(item => item.printDetails());
