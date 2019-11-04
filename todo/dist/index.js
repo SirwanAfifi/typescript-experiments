3ef2db42c25622ab1a4217fcfca3006b9b564324
sirwan <sir1afifi@gmail.com>
2019-11-04 10:51:38 +0330

    Add Inquirer

--- todo/dist/index.js
+++ todo/dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const todoItem_1 = require("./todoItem");
 const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
 let todos = [
     new todoItem_1.TodoItem(1, "Buy Flowers"),
     new todoItem_1.TodoItem(2, "Get Shoes"),
@@ -9,8 +10,34 @@ let todos = [
     new todoItem_1.TodoItem(4, "Call Joe", true)
 ];
 let collection = new todoCollection_1.TodoCollection("Sirwan", todos);
-console.clear();
-console.log(`${collection.userName}'s Todo List ` +
-    `(${collection.getItemCounts().incomplete} items to do)`);
-// collection.removeComplete();
-collection.getTodoItems(true).forEach(item => item.printDetails());
+let showComplete = true;
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List ` +
+        `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(showComplete).forEach(item => item.printDetails());
+}
+var Commands;
+(function (Commands) {
+    Commands["Toggle"] = "Show/Hide Complete";
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer
+        .prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    })
+        .then(answers => {
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showComplete = !showComplete;
+                promptUser();
+                break;
+        }
+    });
+}
+promptUser();
--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from "inquirer";
 
 let todos = [
   new TodoItem(1, "Buy Flowers"),
@@ -9,12 +10,39 @@ let todos = [
 ];
 
 let collection = new TodoCollection("Sirwan", todos);
+let showComplete = true;
 
-console.clear();
-console.log(
-  `${collection.userName}'s Todo List ` +
-    `(${collection.getItemCounts().incomplete} items to do)`
-);
+function displayTodoList(): void {
+  console.log(
+    `${collection.userName}'s Todo List ` +
+      `(${collection.getItemCounts().incomplete} items to do)`
+  );
+  collection.getTodoItems(showComplete).forEach(item => item.printDetails());
+}
 
-// collection.removeComplete();
-collection.getTodoItems(true).forEach(item => item.printDetails());
+enum Commands {
+  Toggle = "Show/Hide Complete",
+  Quit = "Quit"
+}
+
+function promptUser(): void {
+  console.clear();
+  displayTodoList();
+  inquirer
+    .prompt({
+      type: "list",
+      name: "command",
+      message: "Choose option",
+      choices: Object.values(Commands)
+    })
+    .then(answers => {
+      switch (answers["command"]) {
+        case Commands.Toggle:
+          showComplete = !showComplete;
+          promptUser();
+          break;
+      }
+    });
+}
+
+promptUser();
