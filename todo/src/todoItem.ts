d377719758430a88e81c3b53d1b1cc470b9eadfb
sirwan <sir1afifi@gmail.com>
2019-11-04 10:03:10 +0330

    Using more concise sytax

--- todo/src/todoItem.ts
+++ todo/src/todoItem.ts
@@ -1,13 +1,9 @@
 export class TodoItem {
-  public id: number;
-  public task: string;
-  public complete: boolean = false;
-
-  public constructor(id: number, task: string, complete: boolean = false) {
-    this.id = id;
-    this.task = task;
-    this.complete = complete;
-  }
+  public constructor(
+    public id: number,
+    public task: string,
+    public complete: boolean = false
+  ) {}
 
   public printDetails(): void {
     console.log(
