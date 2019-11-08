8e47899970616f10759ec59c873c38675952b941
sirwan <sir1afifi@gmail.com>
2019-11-08 11:23:16 +0330

    catch errors

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,22 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
 });
