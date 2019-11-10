4405bf5c6e162330c597a5fef53d30917d4b1d9f
sirwan <sir1afifi@gmail.com>
2019-11-10 08:53:39 +0330

    Add method

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -11,13 +11,17 @@ interface Todo {
 axios.get(url).then(response => {
   const todo = response.data as Todo;
 
-  const ID = todo.id;
+  const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
-  console.log(`
-    The Todo with ID: ${ID}
-    Has a title of: ${title}
-    Is it finished? ${finished}
-  `);
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id, title, completed) => {
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title of: ${title}
+  Is it finished? ${completed}
+`);
+};
