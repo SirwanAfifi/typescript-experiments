afbf526a3904af07792c680cb50d7da25065c64e
sirwan <sir1afifi@gmail.com>
2019-11-12 09:45:01 +0330

    Add features

---
+++ webapp/src/data/entities.ts
@@ -0,0 +1,59 @@
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+};
+
+export class OrderLine {
+  constructor(public product: Product, public quantity: number) {}
+
+  get total(): number {
+    return this.product.price * this.quantity;
+  }
+}
+
+export class Order {
+  private lines = new Map<number, OrderLine>();
+
+  constructor(initialLines?: OrderLine[]) {
+    if (initialLines) {
+      initialLines.forEach(ol => this.lines.set(ol.product.id, ol));
+    }
+  }
+
+  public addProduct(prod: Product, quatity: number) {
+    if (this.lines.has(prod.id)) {
+      if (quatity === 0) {
+        this.removeProduct(prod.id);
+      } else {
+        this.lines.get(prod.id)!.quantity += quatity;
+      }
+    } else {
+      this.lines.set(prod.id, new OrderLine(prod, quatity));
+    }
+  }
+
+  public removeProduct(id: number) {
+    this.lines.delete(id);
+  }
+
+  get orderLines(): OrderLine[] {
+    return [...this.lines.values()];
+  }
+
+  get productCount(): number {
+    return [...this.lines.values()].reduce(
+      (total, ol) => (total += ol.quantity),
+      0
+    );
+  }
+
+  get total(): number {
+    return [...this.lines.values()].reduce(
+      (total, ol) => (total += ol.total),
+      0
+    );
+  }
+}
