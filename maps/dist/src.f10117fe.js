e958c3b6ee7074418a8774ca87ce84a257e4355c
sirwan <sir1afifi@gmail.com>
2019-11-16 11:19:25 +0330

    Add info window

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -85176,12 +85176,20 @@ function () {
   }
 
   CustomMap.prototype.addMarker = function (mappable) {
-    new google.maps.Marker({
+    var _this = this;
+
+    var marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+    marker.addListener("click", function () {
+      var infoWindow = new google.maps.InfoWindow({
+        content: "Hi there!"
+      });
+      infoWindow.open(_this.googleMap, marker);
     });
   };
 
