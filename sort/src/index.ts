fec0c9a0b4d2b21a31777f3843787db0a1a5bf33
sirwan <sir1afifi@gmail.com>
2019-11-19 21:58:12 +0330

    Add linked list

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharacterCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const characterCollection = new CharacterCollection("Xaayb");
-const sorter = new Sorter(characterCollection);
+// const characterCollection = new CharacterCollection("Xaayb");
+// const sorter = new Sorter(characterCollection);
+// sorter.sort();
+// console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(characterCollection.data);
+console.log(linkedList.print());
