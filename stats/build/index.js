dc764311c8115f37262b5145723ed55325a5515c
sirwan <sir1afifi@gmail.com>
2019-11-19 22:21:52 +0330

    Read CSV file

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,1 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default.readFileSync("football.csv", {
+    encoding: "utf-8"
+});
+console.log(matches);
