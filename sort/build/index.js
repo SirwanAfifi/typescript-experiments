d9709db5a23f78dbafcdfb4fedddf8f06cee59d9
sirwan <sir1afifi@gmail.com>
2019-11-19 11:31:49 +0330

    Add Type Gaurd

--- sort/build/index.js
+++ sort/build/index.js
@@ -7,10 +7,12 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
                 }
             }
         }
