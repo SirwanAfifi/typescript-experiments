b7626758200333426729b84d6fa6ac388a3d3854
sirwan <sir1afifi@gmail.com>
2019-11-19 12:30:19 +0330

    Add Character Collection

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharacterCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const characterCollection = new CharacterCollection("Xaayb");
+const sorter = new Sorter(characterCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(characterCollection.data);
