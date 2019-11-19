29a15a49ace48c206c135e989924b9b7976b16c2
sirwan <sir1afifi@gmail.com>
2019-11-19 10:34:02 +0330

    Add classic sort algorithm

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,20 @@
-console.log(`Hello Sirwan`);
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 0, -5, 3]);
+sorter.sort();
+console.log(sorter.collection);
