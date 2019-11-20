c435d4bf4aa5a4c4214ea09830077777a0045b55
sirwan <sir1afifi@gmail.com>
2019-11-20 11:19:04 +0330

    Add CsvFileReader

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,17 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default
-    .readFileSync("football.csv", {
-    encoding: "utf-8"
-})
-    .split("\n")
-    .map(function (row) {
-    return row.split(",");
-});
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader("football.csv");
+reader.read();
 var matchResult;
 (function (matchResult) {
     matchResult["HomeWin"] = "H";
@@ -19,8 +10,8 @@ var matchResult;
     matchResult["Draw"] = "D";
 })(matchResult || (matchResult = {}));
 var manUnitedWins = 0;
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
     if (match[1] === "Man United" && match[5] === matchResult.HomeWin) {
         manUnitedWins++;
     }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,7 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8"
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const reader = new CsvFileReader("football.csv");
+reader.read();
 
 enum matchResult {
   HomeWin = "H",
@@ -17,7 +11,7 @@ enum matchResult {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === "Man United" && match[5] === matchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === matchResult.AwayWin) {
