8c694bb1b099767a2f44d9c1b8652b7d416ec5af
sirwan <sir1afifi@gmail.com>
2019-11-21 10:26:01 +0330

    Extract map logic

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -17,18 +17,18 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6]
-          ];
-        }
-      );
+      .map(this.mapRow);
+  }
+
+  mapRow(row: string[]): MatchData {
+    return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult,
+      row[6]
+    ];
   }
 }
