3535f9307d0f92c8bc82bc53b7e69c9c5b4c85a3
sirwan <sir1afifi@gmail.com>
2019-11-21 11:46:05 +0330

    Use Generics

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,13 +1,11 @@
 import fs from "fs";
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MatchResult";
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-export class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
 
+  abstract mapRow(row: string[]): T;
   read(): void {
     this.data = fs
       .readFileSync(this.filename, {
@@ -18,17 +16,5 @@ export class CsvFileReader {
         return row.split(",");
       })
       .map(this.mapRow);
-  }
-
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6]
-    ];
   }
 }
