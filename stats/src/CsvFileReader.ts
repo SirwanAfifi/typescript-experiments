3e20fb13c8330c294b3ae22735d6c36f4db1a3a9
sirwan <sir1afifi@gmail.com>
2019-11-22 11:11:27 +0330

    Use Interface approach

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,11 +1,6 @@
 import fs from "fs";
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MatchResult";
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
 export class CsvFileReader {
-  data: MatchData[] = [];
+  data: string[][] = [];
 
   constructor(public filename: string) {}
 
@@ -17,19 +12,6 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
-      })
-      .map(this.mapRow);
-  }
-
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6]
-    ];
+      });
   }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,17 +1,19 @@
-import { CsvFileReader } from "./inheritance/CsvFileReader";
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
+import { MatchReader } from "./MatchReader";
 
-// const reader = new CsvFileReader("football.csv");
-// reader.read();
+const csvFileReader = new CsvFileReader("football.csv");
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-/*for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
-}*/
+}
 
 console.log(`Man United won ${manUnitedWins} games`);
