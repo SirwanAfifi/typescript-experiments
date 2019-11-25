a57e441e887373306335b97afc6b08a07b2e479a
sirwan <sir1afifi@gmail.com>
2019-11-25 10:50:23 +0330

    Add HTML report

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,13 +3,11 @@ import { MatchReader } from "./MatchReader";
 import { Summary } from "./Summary";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis("Man United"),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
