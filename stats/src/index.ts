d5ccf34e909fda2950d1388b59424d5959dae2e0
sirwan <sir1afifi@gmail.com>
2019-11-25 10:38:45 +0330

    Add Analysers

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,15 +5,3 @@ import { MatchReader } from "./MatchReader";
 const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
