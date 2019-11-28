65408aabb613cc8b7b90087fd3e57e64c195a27e
sirwan <sir1afifi@gmail.com>
2019-11-28 10:32:14 +0330

    Add user class

---
+++ web/src/models/User.ts
@@ -0,0 +1,12 @@
+interface UserProps {
+  name: string;
+  age: number;
+}
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+}
