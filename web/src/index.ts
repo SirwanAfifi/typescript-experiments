bf60d7b44a91412e56cbec55b05236ca435485c8
sirwan <sir1afifi@gmail.com>
2019-11-28 10:34:33 +0330

    Instanciate User

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hi there!");
+import { User } from "./models/User";
+
+const user = new User({ name: "Sirwan", age: 30 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
